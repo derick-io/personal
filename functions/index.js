47754a5c7449a27301fe71b842dde76d1f52a29b
derick-io <derickkichize.io@gmail.com>
2022-12-27 17:27:23 -0300

    meta seo updates

--- functions/index.js
+++ functions/index.js
@@ -3,13 +3,16 @@ module.exports = `
   <html lang="en">
   <head>
     <meta charset="UTF-8">
+    <meta name="description" content="Developer in Campinas.">
+    <meta name="keywords" content="HTML, CSS, JavaScript, Campinas, Developer">
+    <meta name="author" content="derick.kichize@gmail.com">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
           rel="stylesheet">
     <script src="https://kit.fontawesome.com/a617890201.js" crossorigin="anonymous"></script>
 
     <script src="/dist/bundle.js"></script>
-    <title>Site test</title>
+    <title>Derick Kichize | Developer</title>
   </head>
   <body></body>
   </html>
